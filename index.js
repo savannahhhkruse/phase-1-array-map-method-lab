e14b16cd9249e68a7030b4ab601ff4bbfeabc827
Savannah <skruse101@gmail.com>
2023-04-23 10:43:55 -0700

    array map method lab

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () =>{
+  return tutorials.map((string) => {
+    const words = string.split(' ')
+    const capitalWord = words.map((word)=>{
+      return word[0].toUpperCase() + word.slice(1)
+    })
+    return capitalWord.join(' ')
+  })
 }
